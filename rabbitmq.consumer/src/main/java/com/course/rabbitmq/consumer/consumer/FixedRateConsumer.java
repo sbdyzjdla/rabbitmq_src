504f1552ec2f2a2010d45c3c7ddde41d1e3a66df
yoonsung <sbdyzjdla@gmail.com>
2022-04-20 20:51:30 +0900

    1개의 큐에 여러개의 소비자

--- rabbitmq.consumer/src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
+++ rabbitmq.consumer/src/main/java/com/course/rabbitmq/consumer/consumer/FixedRateConsumer.java
@@ -6,14 +6,19 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
-//@Service
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@Service
 public class FixedRateConsumer {
 
     private Logger LOG = LoggerFactory.getLogger(FixedRateConsumer.class);
 
-    @RabbitListener(queues = "course.fixedrate")
-    public void listen(String message) {
-        LOG.info("Consuming {}", message);
+    // concurrency = "3-7"  =  최소 3개에서 7개 까지의 동시성생성을 스프링이 관
+    @RabbitListener(queues = "course.fixedrate", concurrency = "3-7")
+    public void listen(String message) throws InterruptedException {
+        TimeUnit.MILLISECONDS.sleep(ThreadLocalRandom.current().nextLong(1000, 2000));
+        LOG.info("{} : Consuming {}", Thread.currentThread().getName() ,message);
     }
 
 }
